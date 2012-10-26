e9e7ffd516ebc3df9a2b59da2a8c5cc08151df71
junkerd <junkerd@lamc-kidd033-13.scf.oregonstate.edu>
2012-10-26 10:49:21 -0700

    start of calculator iOS app

---
+++ calculatorApp/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  calculatorApp
+//
+//  Created by kuwaharg on 10/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ calculatorApp/CalculatorBrain.m
@@ -0,0 +1,34 @@
+//
+//  CalculatorBrain.m
+//  calculatorApp
+//
+//  Created by kuwaharg on 10/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+//Synthesize Getter/Setter (Also set name of backing variables)
+@synthesize operandA = _operandA, operandB = _operandB, operationType = _operationType;
+
+//Implement FUnction
+-(NSNumber *) performOperation{
+    
+    switch (self.operationType) {
+        case addType:
+            return [NSNumber numberWithInt:self.operandA.integerValue + self.operandB.integerValue];
+            break;
+            
+        case subtractType:
+            return [NSNumber numberWithInt:self.operandA.integerValue - self.operandB.integerValue];
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
+}
+
+@end
